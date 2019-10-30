318c346f55dae737f9389b53a33f1f8792fb85e9
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-10-30 20:37:18 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 VERSION="3.0.8"
 
-echo "deb http://in.archive.ubuntu.com/ubuntu/ xenial main" | tee /etc/apt/sources.list.d/xenial.list
+#echo "deb http://in.archive.ubuntu.com/ubuntu/ xenial main" | tee /etc/apt/sources.list.d/xenial.list
 apt-get update
 apt-get --yes install python-software-properties software-properties-common
 add-apt-repository ppa:jonathonf/ffmpeg-4
